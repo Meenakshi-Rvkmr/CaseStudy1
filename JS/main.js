70adb6522be5bf7cfdbf2487cb966057301e3b50
Meenakshi S <93512809+Meenakshi-Rvkmr@users.noreply.github.com>
2021-11-28 19:51:36 +0530

    Added all validations

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,15 @@ function validate() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  if (username != "admin" && password != "12345") {
-    alert("Incorrect username or password");
-  } else {
+  if (username == "admin" && password == "12345") {    
     window.location.href="todo.html";
+  } else {
+    alert("Incorrect username or password");
   }
 }
+
+
+$(document).ready(function () {
+    document.getElementById("loginbutton").addEventListener("click", validate);
+  });
+  
--- JS/todo.js
+++ JS/todo.js
@@ -10,25 +10,43 @@ function ajax() {
       var todoList = JSON.parse(this.response);
 
       var table = document.getElementById("todolist");
-      for (let i = 0; i < todoList.length; i++) {      
+      for (let i = 0; i < todoList.length; i++) {
         let item = todoList[i];
         var checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.name = "name" + item.title;
         checkbox.value = item.title;
-        checkbox.id = item.id;
-        checkbox.checked = item.completed
-        checkbox.disabled = item.completed
-        checkbox.className = "form-check-input"
+        checkbox.id = "checkbox" + item.id;
+        checkbox.checked = item.completed;
+        checkbox.disabled = item.completed;
+        checkbox.className = "form-check-input";
+        checkbox.onchange = function () {
+          countToDos(item.id);
+        };
 
         let row = table.insertRow();
-        let cell1 = row.insertCell()
+        let cell1 = row.insertCell();
         cell1.appendChild(checkbox);
-        let cell2 = row.insertCell()
+        let cell2 = row.insertCell();
         cell2.innerText = item.title;
       }
     }
   };
   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
   xhttp.send();
+}
+
+function logout() {
+  window.location.href = "index.html";
+}
+
+function countToDos(itemid) {
+  let count = document.querySelectorAll(
+    "input[type=checkbox]:checked:enabled"
+  ).length;
+  if (count == 5) {
+    alert("Congrats. 5 Tasks have been Successfully Completed ");
+    // let checkbox = document.getElementById("checkbox" + itemid);
+    // checkbox.checked = false;
+  }
 }
